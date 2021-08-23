096e6c239154758e9cfbfc966174960e5fa3ac05
shubhajeet <shubha.pradhan12@gmail.com>
2021-08-24 01:33:50 +0530

    overview corrected

--- overview/tomby/main.js
+++ overview/tomby/main.js
@@ -1,47 +1,17 @@
-const selection = document.querySelectorAll(".box span"),
-showItem = document.querySelectorAll(".price__box"),
-arrowMove =document.querySelector(".arrow"),
-add = document.querySelector(".add"),
-minus = document.querySelector(".minus");
-let currValue = 1;
-selection.forEach((item) =>{
-    item.addEventListener('click',showdata);
-});
-function showdata(){
-    reset();
-    this.classList.add('active');
-    const dataIndex = Number(this.dataset.index);
-    showItem.forEach((item,index) => {
-        if(dataIndex == index){
-            if(index == 0){
-                arrowMove.style.left = "35px"; 
-            }
-            if(index == 1){
-                arrowMove.style.left = "150px"; 
-            }
-            if(index == 2){
-                arrowMove.style.left = "265px"; 
-            }
-            item.classList.add("show");
-        }
+(function() {
+    var rotate, timeline;
+  
+    rotate = function() {
+      return $('.card:first-child').fadeOut(100, 'swing', function() {
+        return $('.card:first-child').appendTo('.container').hide();
+      }).fadeIn(100, 'swing');
+    };
+  
+    timeline = setInterval(rotate, 1000);
+  
+    $('.card').click(function() {
+      return rotate();
     });
-}
-function reset(){
-    selection.forEach((item) => {
-        item.classList.remove("active")
-    });
-    showItem.forEach((item) => {
-        item.classList.remove("show")
-    });
-}
-add.addEventListener('click',() => {
-    currValue++;
-    document.querySelector(".adding span").innerHTML = currValue;
-});
-minus.addEventListener('click',() => {
-    currValue--;
-    if(currValue <= 0){
-        currValue = 0;
-    }
-    document.querySelector(".adding span").innerHTML = currValue;
-});
+  
+  }).call(this);
+  
